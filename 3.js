28adcda0e9c0aea3510578aa5525b13c559198e1
Gustavo Urbina <gurbina@enkontrol.com>
2023-01-17 20:00:54 -0600

    feat: added increasedMeasurements functionality.

--- 3.js
+++ 3.js
@@ -4,10 +4,18 @@
 // Ejemplo: en el caso de [1, 5, 3, 5] existen 2 incremetos ( el día 2 respecto al dia 1 y el dia 4 respecto al día 3 )
 
 function increasedMeasurements(measurements) {
+  return measurements.reduce(
+    (count, currentElement, index, measurements) =>
+      currentElement > measurements[index - 1] ? count + 1 : count,
+    0
+  );
 }
 
 /**
  * TEST Ejercicio 1
  */
-const measurements = [245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222, 221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234]
-increasedMeasurements(measurements) // 19
+const measurements = [
+  245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222,
+  221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234,
+];
+increasedMeasurements(measurements); // 19
